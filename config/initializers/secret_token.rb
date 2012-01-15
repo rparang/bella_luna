7e1bb3b330fe346ba4c9bbcaf46ea729cf350685
rparang <reza@vitrue.com>
2012-01-14 23:35:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BellaLuna::Application.config.secret_token = 'b8c08553c0c24312b565fae4553d7ae2e3197b6252f8ee7a400e0cd4c16c07053b46dbb068105635a09b10c50d4ac7025a13c75fbea47a11629c7ee1527ba966'
